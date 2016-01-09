63ce0b5afd841e00b5c73ba6c7d9a0dac60e74a0
Takanori Matsumoto <matscube@gmail.com>
2016-01-10 02:10:03 +0900

    Add responder api.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,31 @@
+require 'net/https'
+
 class ApiController < ApplicationController
+	protect_from_forgery :except => :index
+
   def index
+		url = params['url']
+		if url.nil?
+			render :text => "URL Not Received"
+		else
+			# Todo add error handling
+			separator = '/'
+			scheme_separator = '//'
+			parts = url.split(scheme_separator)[1].split(separator)
+
+			host = parts.shift
+			resource = parts.join(separator)
+
+			https = Net::HTTP.new(host, 80)
+			req = Net::HTTP::Get.new(resource)
+			res = https.request(req)
+
+			if res.body.nil?
+				render :text => "Error"
+			else
+				render :text => res.body
+			end
+		end
+
   end
 end
